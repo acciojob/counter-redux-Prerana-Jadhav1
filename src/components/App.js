576066b524c9e77391b4cf090f246d1a0b6aba46
Prerana-Jadhav1 <preranajb5@gmail.com>
2026-09-13 12:22:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../redux/actions";
 import './../styles/App.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.count);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
